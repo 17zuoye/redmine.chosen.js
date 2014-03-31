3648c5a7ae7a9c1ababa347a87c481c7742ff6f2
David Chen <mvjome@gmail.com>
2014-03-31 10:52:34 +0800

    Fix dropdown menu's css

--- redmine.chosen.js
+++ redmine.chosen.js
@@ -3,16 +3,27 @@ $(document).ready(function() {
     // create chosen
     var orig_selector = $("select#issue_assigned_to_id").hide();
     var fake_selector = orig_selector.clone().attr('id', 'fake_issue_assigned_to_id');
-    $.each(fake_selector.children('option'), function(idx, o1) { $(o1).html($(o1).html().split().join()); });
     orig_selector.after(fake_selector);
     // and modiy css
     fake_selector.chosen({width: '200px'});
     var chosen_container = fake_selector.siblings('.chosen-container');
     chosen_container.css('position', 'absolute');
-    chosen_container.find('.chosen-drop').css('position', 'relative');
+    var chosen_drop = chosen_container.find('.chosen-drop');
+
     // and bind event at last.
     fake_selector.on('change', function(evt, params) {
       orig_selector.val(params.selected);
+    });
+
+    // Fix dropdown menu's css
+    fake_selector.on('chosen:ready', function(evt, params) {
+      chosen_drop.hide();
+    });
+    fake_selector.on('chosen:showing_dropdown', function(evt, params) {
+      chosen_drop.show().css('position', 'relative');
+    });
+    fake_selector.on('chosen:hiding_dropdown', function(evt, params) {
+      chosen_drop.hide();
     });
   };
 
@@ -20,7 +31,7 @@ $(document).ready(function() {
   if ( window.location.pathname.match(/\/issues\/(new|[0-9]+)/) ) {
     // Reenable chosen when reload form.
     // The reason is <select updateIssueFrom('/redmine/projects/17zuoye-junior/issues/new.js?id=2739')>
-    setInterval(function() {
+    window.redmine_chosen_interval = setInterval(function() {
      if (!$("#fake_issue_assigned_to_id").length) {
        ensure_chosen();
      }
