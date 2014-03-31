29207d8c9b6f9e3c5040ac114667cad98456a278
David Chen <mvjome@gmail.com>
2014-03-31 10:29:51 +0800

    Reenable chosen when reload form

--- redmine.chosen.js
+++ redmine.chosen.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  // only execute when edit or new an issue
-  if ( window.location.pathname.match(/\/issues\/(new|[0-9]+)/) ) {
+  var ensure_chosen = function() {
     // create chosen
     var orig_selector = $("select#issue_assigned_to_id").hide();
     var fake_selector = orig_selector.clone().attr('id', 'fake_issue_assigned_to_id');
@@ -15,5 +14,16 @@ $(document).ready(function() {
     fake_selector.on('change', function(evt, params) {
       orig_selector.val(params.selected);
     });
+  };
+
+  // only execute when edit or new an issue
+  if ( window.location.pathname.match(/\/issues\/(new|[0-9]+)/) ) {
+    // Reenable chosen when reload form.
+    // The reason is <select updateIssueFrom('/redmine/projects/17zuoye-junior/issues/new.js?id=2739')>
+    setInterval(function() {
+     if (!$("#fake_issue_assigned_to_id").length) {
+       ensure_chosen();
+     }
+    }, 500);
   }
 });
